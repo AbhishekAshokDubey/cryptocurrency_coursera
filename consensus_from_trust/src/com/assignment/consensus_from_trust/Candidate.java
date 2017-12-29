e044a95f55e4453735fde5302ce50600bf84906c
AbhishekAshokDubey <adubey40@gmail.com>
2017-12-30 00:07:08 +0530

    assgn2 1st commit

---
+++ consensus_from_trust/src/com/assignment/consensus_from_trust/Candidate.java
@@ -0,0 +1,11 @@
+package com.assignment.consensus_from_trust;
+
+public class Candidate {
+	Transaction tx;
+	int sender;
+	
+	public Candidate(Transaction tx, int sender) {
+		this.tx = tx;
+		this.sender = sender;
+	}
+}
